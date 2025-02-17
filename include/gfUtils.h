003e38ec2f6889b645e8c63297917221c9c6c1b2
Christopher Poovey <composingcap@gmail.com>
2025-02-16 21:19:38 -0800

    add spat panner
    # Conflicts:
    #	include/gfUtils.h

--- include/gfUtils.h
+++ include/gfUtils.h
@@ -1,6 +1,9 @@
 #pragma once
 #include  <cmath>
 #include <random>
+#include <array>
+#pragma intrinsic(fabs)
+#pragma intrinsic(floor)
 #include "gfEnvelopes.h"
 
 namespace Grainflow
@@ -141,6 +144,12 @@ namespace Grainflow
 				if (input_stream[i - 1] - input_stream[i] < -0.5f) return i;
 			}
 			return block_size;
+		static inline float distance_3d(std::array<float, 3>& a, std::array<float, 3>& b)
+		{
+			auto d0 = b[0] - a[0];
+			auto d1 = b[1] - a[1];
+			auto d2 = b[2] - a[2];
+			return std::sqrtf(d0 * d0 + d1 * d1 + d2 * d2);
 		}
 	};
 
